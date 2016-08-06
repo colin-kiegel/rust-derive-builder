e6fd7cc477b460918f8d8340b5cd8ceaa1386cec
Pascal Hertleif <killercup@gmail.com>
2016-08-07 00:44:58 +0200

    Move macro docs to crates root

--- src/derive_builder.rs
+++ src/derive_builder.rs
@@ -1,56 +1,8 @@
-/// Derive a builder for a struct
+/// `Builder!` macro
 ///
 /// Use in combiantion with [custom_derive][custom_derive].
 ///
 /// [custom_derive]: https://crates.io/crates/custom_derive
-///
-/// # Examples
-///
-/// ```rust
-/// #[macro_use] extern crate custom_derive;
-/// #[macro_use] extern crate derive_builder;
-///
-/// custom_derive!{
-///     #[derive(Debug, PartialEq, Default, Builder)]
-///     struct Lorem {
-///         ipsum: String,
-///         dolor: i32,
-///     }
-/// }
-///
-/// fn main() {
-///     let x = Lorem::default().ipsum("sit").dolor(42);
-///     assert_eq!(x, Lorem { ipsum: "sit".into(), dolor: 42 });
-/// }
-/// ```
-///
-/// ## Generic structs
-///
-/// ```rust
-/// #[macro_use] extern crate custom_derive;
-/// #[macro_use] extern crate derive_builder;
-///
-/// custom_derive!{
-///     #[derive(Debug, PartialEq, Default, Builder)]
-///     struct GenLorem<T> {
-///         ipsum: String,
-///         dolor: T,
-///     }
-/// }
-///
-/// fn main() {
-///     let x = GenLorem::default().ipsum("sit").dolor(42);
-///     assert_eq!(x, GenLorem { ipsum: "sit".into(), dolor: 42 });
-/// }
-/// ```
-///
-/// ## Gotchas
-///
-/// - Tuple structs and unit structs are not supported as they have no field
-///   names.
-/// - When defining a generic struct, you cannot use `VALUE` as a generic
-///   parameter as this is what all setters are using.
-///
 #[macro_export]
 macro_rules! Builder {
     // Strip empty argument list if given (Passed by custom_derive macro)
