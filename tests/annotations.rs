aa37c8ad0b9c9d490467d714e17343d3793d93c7
Colin Kiegel <kiegel@gmx.de>
2016-08-13 17:01:21 +0200

    fix failing tests on stable

--- tests/annotations.rs
+++ tests/annotations.rs
@@ -4,8 +4,8 @@
 custom_derive!{
     /// This is a doc comment for the struct
     #[warn(missing_docs)]
-    #[allow(non_snake_case)]
-    #[derive(Debug, PartialEq, Default, Builder)]
+    #[allow(non_snake_case, dead_code)]
+    #[derive(Builder)]
     struct Lorem {
         /// This is a doc comment for a field
         field_with_doc_comment: String,
