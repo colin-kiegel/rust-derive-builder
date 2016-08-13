5cc0c0354da2f134609337128bfc34f242bea06f
Colin Kiegel <kiegel@gmx.de>
2016-08-13 16:56:18 +0200

    more tests

--- tests/annotations.rs
+++ tests/annotations.rs
@@ -4,13 +4,24 @@
 custom_derive!{
     /// This is a doc comment for the struct
     #[warn(missing_docs)]
+    #[allow(non_snake_case)]
     #[derive(Debug, PartialEq, Default, Builder)]
     struct Lorem {
         /// This is a doc comment for a field
         field_with_doc_comment: String,
         #[allow(missing_docs)]
         undocumented: String,
+        #[allow(non_snake_case)]
+        CamelCase: i32,
+        #[cfg(target_os = "macos")] // TODO: this fails on linux builds
+        mac_only: (),
+        #[cfg(target_os = "linux")] // TODO: this fails on mac builds
+        linux_only: (),
     }
 }
 
-// this is just a compile-test - no run time checks required.
+#[test]
+fn annotations() {
+    // this is currently just a compile-test (may switch to token comparisons here)
+    // https://github.com/colin-kiegel/rust-derive-builder/issues/19
+}
