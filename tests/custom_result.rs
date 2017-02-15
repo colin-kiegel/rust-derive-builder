a8c0c21525c2ba9362cc1ae7138b70ba55b60e45
0g <mail@0g.net>
2017-02-16 01:41:16 +0300

    Test for #39
    Add test for #39

---
+++ tests/custom_result.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+extern crate derive_builder;
+
+type Result = ();
+
+#[allow(dead_code)]
+#[derive(Builder)]
+struct IgnoreEmptyStruct {}
+
+#[test]
+fn empty_struct() {
+    // this is just a compile-test - no run time checks required.
+}
