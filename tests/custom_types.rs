da69ceb53d00234a1d4ab2239a227704ebc27b2c
0g <mail@0g.net>
2017-02-16 13:20:35 +0300

    Use full path for Clone, Into and Option also.

--- tests/custom_types.rs
+++ tests/custom_types.rs
@@ -1,9 +1,13 @@
+#![allow(dead_code)]
+
 #[macro_use]
 extern crate derive_builder;
 
+type Clone = ();
+type Into = ();
+type Option = ();
 type Result = ();
 
-#[allow(dead_code)]
 #[derive(Builder)]
 struct IgnoreEmptyStruct {}
 
