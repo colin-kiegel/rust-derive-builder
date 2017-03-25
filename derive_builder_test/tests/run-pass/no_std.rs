093939f5e85da27022ccc7a0c600bfd51d64f0fa
Colin Kiegel <kiegel@gmx.de>
2017-03-25 20:29:07 +0100

    fix compiletest

--- derive_builder_test/tests/run-pass/no_std.rs
+++ derive_builder_test/tests/run-pass/no_std.rs
@@ -1,6 +1,6 @@
 // requires nightly toolchain!
 #![no_std]
-#![feature(collections)]
+#![feature(collections, lang_items, start)]
 #![allow(dead_code)]
 
 #[macro_use]
@@ -10,3 +10,20 @@ extern crate collections;
 #[derive(Builder)]
 #[builder(no_std)]
 struct IgnoreEmptyStruct {}
+
+///////////////////////////////////////////////////////////////
+// some no_std-boilerplate
+// from https://doc.rust-lang.org/book/no-stdlib.html
+///////////////////////////////////////////////////////////////
+
+// These functions and traits are used by the compiler, but not
+// for a bare-bones hello world. These are normally
+// provided by libstd.
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
+
+// Entry point for this program
+#[start]
+fn start(_argc: isize, _argv: *const *const u8) -> isize {
+    0
+}
