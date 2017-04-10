0b59e93d5e76871fd24ca620bf40683dee6fbe76
Ted Driggs <ted.driggs@outlook.com>
2017-04-10 13:11:36 -0700

    Fixed a dead_code warning in the try_setter test

--- derive_builder/tests/try_setter.rs
+++ derive_builder/tests/try_setter.rs
@@ -54,6 +54,8 @@ mod struct_level {
         assert_eq!(built, exact_helper().unwrap());
     }
 
+    // Allow dead code here since the test that uses this depends on the try_setter feature.
+    #[cfg_attr(not(feature = "try_setter"), allow(dead_code))]
     fn exact_helper() -> Result<Lorem, String> {
         LoremBuilder::default()
             .source(IpAddr::from_str("1.2.3.4").unwrap())
