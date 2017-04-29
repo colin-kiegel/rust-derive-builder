6e9f47900a31805e2967d37ad3784f18a8b8103b
Colin Kiegel <kiegel@gmx.de>
2017-04-29 12:59:17 +0200

    travis deploy hook (on tags)

---
+++ dev/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function main {
+  export CARGO_TARGET_DIR="../.deploy/"
+
+  if [ -z "$CRATES_IO_TOKEN" ]; then
+    echo "EE: The variable \`CRATES_IO_TOKEN\` must be set!"
+    exit 1;
+  fi
+
+  (cd derive_builder_core && rustup run nightly cargo publish --token "$CRATES_IO_TOKEN")
+  (cd derive_builder      && rustup run nightly cargo publish --token "$CRATES_IO_TOKEN")
+}
+
+function base_dir {
+  if [ $(uname -s) != "Darwin" ] && hash readlink 2>/dev/null; then
+    # use readlink, if installed, to follow symlinks
+    local __DIR="$(dirname "$(readlink -f "$0")")"
+  else
+    local __DIR="$(dirname "$0")"
+  fi
+  echo ${__DIR}
+}
+
+# cd into the crate root
+(cd "$(base_dir)/.." && main $@)
