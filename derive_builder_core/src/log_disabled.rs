7d1188937b0c3887ae56e7188e74821d00c455f3
Colin Kiegel <kiegel@gmx.de>
2017-06-30 10:31:30 +0200

    fix unused macro test regression

--- derive_builder_core/src/log_disabled.rs
+++ derive_builder_core/src/log_disabled.rs
@@ -2,6 +2,7 @@
 ///
 /// Source shared by `derive_builder_core` and `derive_builder` via symlink.
 
+#[allow(unknown_lints, unused_macros)]
 macro_rules! log_enabled {
     ($( $x:tt )*) => { false }
 }
@@ -9,26 +10,32 @@ macro_rules! log_enabled {
 // delegate to format_args and throw away the result to avoid `unused variable`
 // lints.
 // The compiler should be able to optimize this away.
+#[allow(unknown_lints, unused_macros)]
 macro_rules! debug {
     ($( $x:tt )*) => { format_args!($( $x )*); }
 }
 
+#[allow(unknown_lints, unused_macros)]
 macro_rules! error {
     ($( $x:tt )*) => { format_args!($( $x )*); }
 }
 
+#[allow(unknown_lints, unused_macros)]
 macro_rules! info {
     ($( $x:tt )*) => { format_args!($( $x )*); }
 }
 
+#[allow(unknown_lints, unused_macros)]
 macro_rules! log {
     ($( $x:tt )*) => { format_args!($( $x )*); }
 }
 
+#[allow(unknown_lints, unused_macros)]
 macro_rules! trace {
     ($( $x:tt )*) => { format_args!($( $x )*); }
 }
 
+#[allow(unknown_lints, unused_macros)]
 macro_rules! warn {
     ($( $x:tt )*) => { format_args!($( $x )*); }
 }
