f15745c1817c0374be1b3f990261a444a3481260
Azriel Hoh <azriel91@gmail.com>
2018-03-13 10:59:55 +1300

    Inform user if their git config contains `hooks.checkfeatures`
    This also tells them that the flag has been updated to
    `hooks.checkminimumrust`.

--- dev/githook.sh
+++ dev/githook.sh
@@ -101,6 +101,7 @@ function load_config {
 	lookup_git_flag checknightly
 	lookup_git_flag nightlytests
 	lookup_git_flag checkminimumrust
+	obsolete_git_flag checkfeatures '`hooks.checkfeatures` has been replaced by `hooks.checkminimumrust`'
 
   if [ $config_status -ne 0 ]; then
 		echo -e "${FMT_ERR}EE${FMT_RESET}: Invalid git configuration. Aborting checks."
@@ -140,6 +141,19 @@ function lookup_git_flag {
 		>&2 echo
 
 		config_status=1
+	fi
+}
+
+function obsolete_git_flag {
+	# lookup the boolean value of 'hooks.$1' (i.e. true or false)
+	# if it is set, then we inform the user that the flag is no longer in use
+	local flag="$(git config --bool hooks.$1)"
+	if [ "$flag" == true ] || [ "$flag" == false ]; then
+		[ -n "$2" ] && >&2 echo -e "${FMT_INFO}II${FMT_RESET}: $2"
+		>&2 echo -e "${FMT_INFO}II${FMT_RESET}: obsolete git flag found: hooks.$1"
+		>&2 echo "    you can remove it like"
+		>&2 echo "  $ git config --unset hooks.$1"
+		>&2 echo
 	fi
 }
 
