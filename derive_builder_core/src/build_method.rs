4cd3f3291b0aa0bb31b2a6da0e076aa3b434a236
Ted Driggs <ted.driggs@outlook.com>
2021-01-15 10:20:20 -0800

    Attach validation-related errors to better span
    If the build() method cannot convert the validation error to the
    generated builder error, this is a problem with the validation function.
    The error span should point there, not at the macro call-site.
    
    Due to some issue in `darling` or `syn`, this change is pointing at the
    closing quotation instead of the whole path; that is being tracked as
    TedDriggs/darling#120.

--- derive_builder_core/src/build_method.rs
+++ derive_builder_core/src/build_method.rs
@@ -2,6 +2,7 @@ use doc_comment_from;
 use proc_macro2::{Span, TokenStream};
 use quote::{ToTokens, TokenStreamExt};
 use syn;
+use syn::spanned::Spanned;
 use Block;
 use BuilderPattern;
 use Initializer;
@@ -81,7 +82,10 @@ impl<'a> ToTokens for BuildMethod<'a> {
             let ident = syn::Ident::new(DEFAULT_STRUCT_NAME, Span::call_site());
             quote!(let #ident: #target_ty #target_ty_generics = #default_expr;)
         });
-        let validate_fn = self.validate_fn.as_ref().map(|vfn| quote!(#vfn(&self)?;));
+        let validate_fn = self
+            .validate_fn
+            .as_ref()
+            .map(|vfn| quote_spanned!(vfn.span() => #vfn(&self)?;));
         let error_ty = &self.error_ty;
 
         if self.enabled {
