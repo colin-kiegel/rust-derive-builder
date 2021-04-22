dd94e05e1618b94b6f023c8b9e250788e72a1a59
Andrey Kutejko <andy128k@gmail.com>
2021-04-22 16:31:31 +0200

    Re-introduce test crate for no_std environment (#208)
    It was a binary crate and now it is a library crate.
    This "test" is less strict than the previous one. 
    We may resurrect the old one (binary), but it will work on nightly only.

---
+++ derive_builder_no_std_tests/src/lib.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![allow(unused)]
+
+extern crate alloc;
+#[macro_use]
+extern crate derive_builder;
+
+use alloc::string::String;
+use alloc::string::ToString;
+
+#[derive(Builder)]
+#[builder(no_std)]
+pub struct Foo {
+    pub bar: i32,
+}
+
+pub fn build_foo_ok() -> Foo {
+    FooBuilder::default().bar(42).build().unwrap()
+}
+
+pub fn build_foo_err() -> Option<String> {
+    let foo = FooBuilder::default().build();
+    foo.err().map(|err| err.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_builder_ok() {
+        assert_eq!(build_foo_ok().bar, 42);
+    }
+
+    #[test]
+    fn test_builder_err() {
+        assert_eq!(
+            build_foo_err().as_deref(),
+            Some("`bar` must be initialized")
+        );
+    }
+}
