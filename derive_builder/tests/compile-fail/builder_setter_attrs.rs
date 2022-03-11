ee9bd0894caa5cf712f741077519061716d54d13
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-03-11 11:59:00 +0000

    Test builder_setter_attr(...)

---
+++ derive_builder/tests/compile-fail/builder_setter_attrs.rs
@@ -0,0 +1,19 @@
+#![deny(unused_must_use)]
+
+#[macro_use]
+extern crate derive_builder;
+
+#[derive(Debug, PartialEq, Default, Builder, Clone)]
+pub struct Lorem {
+    ok: String,
+
+    // This foolish repr() attribute generates an unused attribute warning
+    #[builder_setter_attrs(
+        #[must_use]
+    )]
+    broken: usize,
+}
+
+fn main() {
+    LoremBuilder::default().broken(42);
+}
