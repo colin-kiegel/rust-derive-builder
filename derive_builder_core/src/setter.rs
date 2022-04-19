f7b57f83db0ef388c80369ebac9a2bca19bd3ca3
Ian Jackson <ijackson+github@slimy.greenend.org.uk>
2022-04-19 13:10:11 +0100

    Genericise wrap_expression_in_some
    Co-authored-by: Ted Driggs <ted.driggs@outlook.com>

--- derive_builder_core/src/setter.rs
+++ derive_builder_core/src/setter.rs
@@ -226,7 +226,7 @@ impl<'a> ToTokens for Setter<'a> {
 }
 
 /// Returns expression wrapping `bare_value` in `Some`
-fn wrap_expression_in_some(bare_value: TokenStream) -> TokenStream {
+fn wrap_expression_in_some(bare_value: impl ToTokens) -> TokenStream {
     quote!( ::derive_builder::export::core::option::Option::Some(#bare_value) )
 }
 
