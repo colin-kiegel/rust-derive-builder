e567abf8b1004f83c26ddff5f466169f5d240680
Ted Driggs <ted.driggs@outlook.com>
2024-02-14 13:52:55 -0800

    Add updated publish script
    The previous one relies on CI automation that cannot be maintained without the repo owner, who is absent.
    
    The new script can be run locally by any authorized maintainer.

---
+++ dev/publish.sh
@@ -0,0 +1,3 @@
+cargo publish --manifest-path derive_builder_core/Cargo.toml &&
+cargo publish --manifest-path derive_builder_macro/Cargo.toml &&
+cargo publish --manifest-path derive_builder/Cargo.toml;
