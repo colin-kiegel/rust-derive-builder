6398607268bceff60d25b15b0c56f6be3661fef0
Ted Driggs <ted.driggs@outlook.com>
2025-08-04 09:36:29 -0700

    Fix new clippy violation

--- derive_builder_core/src/builder_field.rs
+++ derive_builder_core/src/builder_field.rs
@@ -86,8 +86,7 @@ impl<'a> BuilderFieldType<'a> {
     /// Obtain type information for the builder field setter
     ///
     /// Return value:
-    ///  * `.0`: type of the argument to the setter function
-    ///          (before application of `strip_option`, `into`)
+    ///  * `.0`: type of the argument to the setter function (before application of `strip_option`, `into`)
     ///  * `.1`: whether the builder field is `Option<type>` rather than just `type`
     pub fn setter_type_info(&'a self) -> (&'a syn::Type, bool) {
         match self {
